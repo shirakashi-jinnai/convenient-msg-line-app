b73065e8bd960b06b57924b77e691a8f46b3cd7d
shirakashi-jinnai <j.shirakashi.jb@gmail.com>
2022-02-01 21:06:29 +0900

    test

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const https = require("https");
 const bodyparser = require("body-parser");
 const line = require("@line/bot-sdk");
 const res = require("express/lib/response");
 const app = express();
-const port = process.env.PORT || "3000";
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const config = {
   channelAccessToken: process.env.LINE_ACCESS_TOKEN,
@@ -17,6 +21,19 @@ app.post("/webhook", line.middleware(config), (req, res) => {
   );
 });
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+const client = new line.Client(config);
+
+const handleEvent = (event) => {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: event.message.text,
+  });
+};
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
 });
